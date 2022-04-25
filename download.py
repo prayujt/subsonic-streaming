398ec704b3c178e64b6803ab9b374cdd62b4474d
Prayuj Tuli <prayujtuli@hotmail.com>
2022-04-25 02:20:03 -0400

    Completed artist downloading

--- download.py
+++ download.py
@@ -11,29 +11,40 @@ import requests
 import json
 
 def clean(value):
-    value = value.replace('\'','').replace('\"','').replace('’','')
+    value = value.replace('\'','').replace('\"','').replace('’','').replace('$','S')
     return value
 
-def get_video(track, album, artist):
-    track = clean(track)
-    album = clean(album)
-    artist = clean(artist)
-    print(track, album, artist)
-    query = track + ' ' + artist + ' lyrics'
+def get_video(track, album, artist, release_date, track_num, cover_photo):
+    new_track = clean(track)
+    new_album = clean(album)
+    new_artist = clean(artist)
+    query = new_track + ' ' + new_artist + ' lyrics'
     query = clean(query).replace(' ', '+')
     print(query)
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + query)
     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
     video_id = video_ids[0]
-    artistExists = os.path.isdir('/home/files/Music/{0}'.format(artist))
-    albumExists = os.path.isdir('/home/files/Music/{0}/{1}'.format(artist, album))
+    artistExists = os.path.isdir('/home/files/Music/{0}'.format(new_artist))
+    albumExists = os.path.isdir('/home/files/Music/{0}/{1}'.format(new_artist, new_album))
     if not artistExists:
-        os.system('mkdir \"/home/files/Music/{0}\"; mkdir \"/home/files/Music/{0}/{1}\"'.format(artist, album))
+        os.system('mkdir \"/home/files/Music/{0}\"; mkdir \"/home/files/Music/{0}/{1}\"'.format(new_artist, new_album))
     else:
         if not albumExists:
-            os.system('mkdir \"/home/files/Music/{0}/{1}\"'.format(artist, album))
+            os.system('mkdir \"/home/files/Music/{0}/{1}\"'.format(new_artist, new_album))
+    location = '/home/files/Music/{0}/{1}'.format(new_artist, new_album)
+    os.system('node /home/files/.scripts/music/youtube_mp3.js {0} \"{1}.mp3\" \"{2}\"'.format(video_id,track,location))
 
-    #os.system('node /home/files/.scripts/music/youtube_mp3.js {0} \"{1}.mp3\"'.format(video_id,track))ko
+    file_location = location + '/' + track + '.mp3'
+    audiofile = eyed3.load(file_location)
+    audiofile.tag.title = track
+    audiofile.tag.album = album
+    audiofile.tag.artist = artist
+    audiofile.tag.release_date = release_date
+    audiofile.tag.recording_date = release_date
+    audiofile.tag.track = track_num
+    # audiofile.tag.images.set(img_url=cover_photo)
+
+    audiofile.tag.save()
 
 def download_track(id_):
     r = requests.get('https://api.spotify.com/v1/tracks/{0}'.format(id_), headers={
@@ -77,9 +88,12 @@ def download_artist(id_):
 
     for album in response['albums']:
         album_name = album['name']
+        release_date = album['release_date']
         artist = album['artists'][0]['name']
+        cover_art = album['images'][0]['url']
         for track in album['tracks']['items']:
-            get_video(track['name'], album['name'], artist)
+            track_num = track['track_number']
+            get_video(track['name'], album['name'], artist, release_date, track_num, cover_art)
 
 music = []
 
@@ -125,7 +139,6 @@ for value in music:
 """
 
 
-audiofile = eyed3.load("/home/files/Music/" + title + ".mp3")
 
 try:
     album = response['tracks']['items'][0]['album']['name']
