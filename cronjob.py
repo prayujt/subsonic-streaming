8fee524f92396f5b30dc5eb431a9db3083b83822
Prayuj Tuli <prayujtuli@hotmail.com>
2022-06-20 16:41:55 -0400

    Fixed playlist migration bugs, added better youtube downloading

--- cronjob.py
+++ cronjob.py
@@ -10,6 +10,6 @@ client = ampache.API()
 passphrase = client.encrypt_string(config['API_KEY'], 'prayuj')
 auth = client.handshake('http://prayujt.com:1025', passphrase)
 
-client.catalog_action('add_to_catalog', catalog_id)
-client.catalog_action('verify_catalog', catalog_id)
 client.catalog_action('clean_catalog', catalog_id)
+client.catalog_action('verify_catalog', catalog_id)
+client.catalog_action('add_to_catalog', catalog_id)
--- download.py
+++ download.py
@@ -40,7 +40,6 @@ class Download:
         new_album = self.clean(album)
         new_artist = self.clean(artist)
         query = self.clean(new_track + ' ' + new_artist + ' audio').replace('  ', '+').replace(' ' , '+').replace('&', '%26')
-        print(query)
         try:
             html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + query)
         except UnicodeEncodeError as e:
@@ -67,12 +66,11 @@ class Download:
         else:
             if not albumExists:
                 os.system('mkdir \"/home/files/Music/{0}/{1}\"'.format(new_artist, new_album))
-        location = '/home/files/Music/{0}/{1}'.format(new_artist, new_album)
-        os.system('node /home/files/.scripts/music/youtube_mp3.js {0} \"{1}.mp3\" \"{2}\"'.format(video_id,new_track,location))
+        file_location = '/home/files/Music/{0}/{1}/{2}.mp3'.format(new_artist, new_album, new_track)
+        os.system("/home/files/.local/bin/yt-dlp -f \"ba\" -x --audio-format mp3 https://www.youtube.com/watch?v={0} -o \"{1}\"".format(video_id, file_location))
+        #os.system('node /home/files/.scripts/music/youtube_mp3.js {0} \"{1}.mp3\" \"{2}\"'.format(video_id,new_track,location))
 
-        file_location = location + '/' + new_track + '.mp3'
         if os.path.isfile(file_location):
-            print(file_location)
             try:
                 audiofile = eyed3.load(file_location)
             except Exception as e:
@@ -88,11 +86,12 @@ class Download:
             audiofile.tag.save()
 
             try:
-                self.client.catalog_action('add_to_catalog', self.catalog_id)
-                self.client.catalog_action('verify_catalog', self.catalog_id)
-                self.client.catalog_action('clean_catalog', self.catalog_id)
+                self.client.catalog_file(file_location, 'add', self.catalog_id)
+                self.client.catalog_action('gather_art', self.catalog_id)
             except json.decoder.JSONDecodeError:
+                print('failed to add to catalog')
                 pass
+            return file_location
 
     def download_track(self, id_):
         r = requests.get('https://api.spotify.com/v1/tracks/{0}'.format(id_), headers={
@@ -103,9 +102,7 @@ class Download:
         album_name = response['album']['name']
         release_date = response['album']['release_date']
         artist_name = response['album']['artists'][0]['name']
-        self.get_video(response['name'], album_name, artist_name, release_date, response['track_number'], '')
-        #time.sleep(1)
-        #self.client.catalog_action('verify_catalog', self.catalog_id)
+        return self.get_video(response['name'], album_name, artist_name, release_date, response['track_number'], '')
 
     def download_album(self, id_):
         r = requests.get('https://api.spotify.com/v1/albums/{0}'.format(id_), headers={
@@ -121,8 +118,6 @@ class Download:
 
         for track in response['tracks']['items']:
             self.get_video(track['name'], album_name, artist_name, release_date, track['track_number'], '')
-        time.sleep(1)
-        self.client.catalog_action('verify_catalog', self.catalog_id)
 
     def download_artist(self, id_):
         albums = ''
@@ -152,8 +147,6 @@ class Download:
             for track in album['tracks']['items']:
                 track_num = track['track_number']
                 self.get_video(track['name'], album_name, artist_name, release_date, track_num, '')
-        time.sleep(1)
-        self.client.catalog_action('verify_catalog', self.catalog_id)
 
     def download_hindi(self, music_type, id_):
         if music_type == 'track':
@@ -169,7 +162,7 @@ class Download:
             sys.exit()
 
         time.sleep(1)
-        self.client.catalog_action('verify_catalog', self.catalog_id)
+        self.client.catalog_action('add_to_catalog', self.catalog_id)
 
     def download_track_manual(self, spotify_url, youtube_url):
         id_ = spotify_url[spotify_url.find('track/')+6:]
@@ -198,22 +191,30 @@ class Download:
             os.system('rm \"{0}\"'.format(filePath))
 
         self.get_video(track_name, album_name, artist_name, release_date, track_number, youtube_url[youtube_url.find('v=')+2:])
-        time.sleep(1)
-        self.client.catalog_action('verify_catalog', self.catalog_id)
 
     def playlist_loop(self, playlist, playlist_id):
         for song in playlist:
+            print(song['track']['name'])
             album = song['track']['album']['name']
             songs = self.client.songs(filter_str=song['track']['name'], exact=1)
+            print(songs)
             found = False
             for temp in songs['song']:
                 if temp['album']['name'] == album:
                     found = True
                     self.client.playlist_add_song(playlist_id, temp['id'], 1)
+                    break
             if not found:
                 href = song['track']['href']
-                self.download_track(href[href.find('tracks/')+7:])
+                path = self.download_track(href[href.find('tracks/')+7:])
+                if path == None:
+                    print('Error')
+                    continue
+                print(path)
                 songs = self.client.songs(filter_str=song['track']['name'], exact=1)
+                while len(songs['song']) == 0:
+                    result = self.client.catalog_file(path, 'add', self.catalog_id)
+                    songs = self.client.songs(filter_str=song['track']['name'], exact=1)
                 for temp in songs['song']:
                     if temp['album']['name'] == album:
                         found = True
